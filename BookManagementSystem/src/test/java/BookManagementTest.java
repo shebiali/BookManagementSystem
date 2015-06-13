de4036099ce026a3ee25a5cbac583007d6b62201
shahbaz <shebi.ali@gmail.com>
2015-06-13 21:10:16 +0400

    test/java/BookManagementTest.java added

---
+++ BookManagementSystem/src/test/java/BookManagementTest.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+
+import com.bms.model.Book;
+
+/**
+ * 
+ */
+
+/**
+ * @author Shahbaz
+ *
+ */
+public class BookManagementTest {
+
+	@Test
+	public void testAddBook(){
+		
+		
+	}
+	
+}
